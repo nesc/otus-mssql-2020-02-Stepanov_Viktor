37668673e65c3487cbee4115f776fb2b930c972f
unknown <nesc.fire@gmail.com>
2020-03-17 08:29:54 +0300

    Доделал домашнее задание 5
    Скрипт по созданию БД, схемы, таблиц и индексов. Скриншот созданных объектов приложен.

--- Homework-5/Homework_5_DDL.sql
+++ Homework-5/Homework_5_DDL.sql
@@ -55,79 +55,104 @@ Create schema bi;
 GO
 
 Create table bi.ClientType (ClientTypeID	int primary key
-                           ,Brief			char(20)
-						   ,Name			char(160)
+                           ,Brief			varchar(20)
+						   ,Name			varchar(160)
                            );
+						   
+create index Index_Brief on bi.ClientType(Brief);
 
 Create table bi.Client (ClientID		int primary key
                        ,ClientTypeID	int not null foreign key references bi.ClientType(ClientTypeID)
-					   ,Brief			char(25)
-					   ,FirstName		char(160)
-                       ,LastName		char(160)
-                       ,MiddleName		char(160)
+					   ,Brief			varchar(25)
+					   ,FirstName		varchar(160)
+                       ,LastName		varchar(160)
+                       ,MiddleName		varchar(160)
 				   	   ,Sex				int
 					   ,BirthDate		smalldatetime
-					   ,INN				char(20)
-
+					   ,INN				varchar(20)
                         );
+						   
+create index Index_Type_Brief on bi.Client(ClientTypeID, Brief);
+create index Index_INN on bi.Client(INN);
 
 Create table bi.ClientDocType (ClientDocTypeID	int primary key
-                              ,Brief			char(20)
-						      ,Name				char(160)
+                              ,Brief			varchar(20)
+						      ,Name				varchar(160)
                               );
+						   
+create index Index_Brief on bi.ClientDocType(Brief);
 
 Create table bi.ClientDoc (ClientDocID		int primary key
 					      ,ClientID			int not null foreign key references bi.Client(ClientID)
 						  ,ClientDocTypeID	int not null foreign key references bi.ClientDocType(ClientDocTypeID)
-                          ,DocNumber		char(20)
-                          ,DocSeries		char(20)
-					      ,Code				char(20)
+                          ,DocNumber		varchar(20)
+                          ,DocSeries		varchar(20)
+					      ,Code				varchar(20)
 						  ,DateStart		smalldatetime
 						  ,DateEnd			smalldatetime
-						  ,DocPlace			char(255)
+						  ,DocPlace			varchar(255)
 						  ,Active			int
                           );
+						   
+create index Index_ClientID on bi.ClientDoc(ClientID);
+create index Index_ClientDocTypeID on bi.ClientDoc(ClientDocTypeID);
+create index Index_DocNumber_DocSeries on bi.ClientDoc(DocNumber,DocSeries);
 
 Create table bi.ContactType (ContactTypeID	int primary key
-                            ,Brief			char(20)
-						    ,Name			char(160)
+                            ,Brief			varchar(20)
+						    ,Name			varchar(160)
                             );
+						   
+create index Index_Brief on bi.ContactType(Brief);
 
 Create table bi.ClientContact (ClientContactID	int primary key
                               ,ClientID			int not null foreign key references bi.Client(ClientID)
 							  ,ContactTypeID	int not null foreign key references bi.ContactType(ContactTypeID)
-							  ,Value			char(30)
+							  ,Value			varchar(30)
 							  ,Active			int
                                );
+						   
+create index Index_ClientID on bi.ClientContact(ClientID);
+create index Index_ContactTypeID on bi.ClientContact(ContactTypeID);
 
 Create table bi.AddressType (AddressTypeID	int primary key
-                            ,Brief			char(20)
-						    ,Name			char(160)
+                            ,Brief			varchar(20)
+						    ,Name			varchar(160)
                             );
+						   
+create index Index_Brief on bi.AddressType(Brief);
 
 Create table bi.ClientAddress (ClientAddressID	int primary key
                               ,ClientID			int not null foreign key references bi.Client(ClientID)
 							  ,AddressTypeID	int not null foreign key references bi.AddressType(AddressTypeID)
-							  ,Value			char(200)
+							  ,Value			varchar(200)
 							  ,Active			int
                                );
+						   
+create index Index_ClientID on bi.ClientAddress(ClientID);
+create index Index_AddressTypeID on bi.ClientAddress(AddressTypeID);
 
 Create table bi.Balance (BalanceID	int primary key
-                        ,Brief		char(20)
-						,Name		char(160)
+                        ,Brief		varchar(20)
+						,Name		varchar(160)
                         );
+						   
+create index Index_Brief on bi.Balance(Brief);
 
 Create table bi.Account (AccountID	int primary key
                         ,ClientID	int not null foreign key references bi.Client(ClientID)
 						,BankID		int not null foreign key references bi.Client(ClientID)
-						,Number		char(20)
-						,Name		char(100)
+						,BalanceID	int not null foreign key references bi.Balance(BalanceID)
+						,Number		varchar(20)
+						,Name		varchar(100)
 						,Currency	int
 						,DateStart	smalldatetime
 						,DateEnd	smalldatetime
 						,Active		int
-						,BalanceID	int not null foreign key references bi.Balance(BalanceID)
                          );
+						   
+create index Index_ClientID on bi.Account(ClientID);
+create index Index_Number on bi.Account(Number);
 
 Create table bi.Rest (RestID	int primary key
                      ,AccountID	int not null foreign key references bi.Account(AccountID)
@@ -135,21 +160,30 @@ Create table bi.Rest (RestID	int primary key
 					 ,Amount	money
 					 ,Date		smalldatetime
                      );
+						   
+create index Index_AccountID_Date on bi.Rest(AccountID, Date);
 
 Create table bi.Instrument (InstrumentID	int primary key
-                           ,Brief			char(20)
-						   ,Name			char(160)
+                           ,Brief			varchar(20)
+						   ,Name			varchar(160)
                            );
+						   
+create index Index_Brief on bi.Instrument(Brief);
 
-Create table bi.State (StateID	int primary key
-                      ,Brief	char(20)
-				      ,Name		char(160)
+Create table bi.State (StateID			int primary key
+					  ,InstrumentID		int not null foreign key references bi.Instrument(InstrumentID)
+                      ,Brief			varchar(20)
+				      ,Name				varchar(160)
                       );
+						   
+create index Index_Brief on bi.State(Brief);
 
 Create table bi.BankProduct (BankProductID	int primary key
-                            ,Brief			char(20)
-				            ,Name			char(160)
+                            ,Brief			varchar(20)
+				            ,Name			varchar(160)
                             );
+						   
+create index Index_Brief on bi.BankProduct(Brief);
 
 Create table bi.ContractCredit (ContractCreditID	int primary key
 							   ,InstrumentID		int not null foreign key references bi.Instrument(InstrumentID)
@@ -157,7 +191,7 @@ Create table bi.ContractCredit (ContractCreditID	int primary key
                                ,BankProductID		int not null foreign key references bi.BankProduct(BankProductID)
 							   ,StateID				int not null foreign key references bi.State(StateID)
 							   ,BankID              int not null foreign key references bi.Client(ClientID)
-							   ,Number              char(40)
+							   ,Number              varchar(40)
 					           ,Currency			int
 					           ,CreditAmount		money
 					           ,CreditDateFrom		smalldatetime
@@ -165,6 +199,10 @@ Create table bi.ContractCredit (ContractCreditID	int primary key
 							   ,PaymentDay			int
 							   ,Active				int
                                );
+						   
+create index Index_ClientID on bi.ContractCredit(ClientID);
+create index Index_InstrumentID_StateID on bi.ContractCredit(InstrumentID, StateID);
+create index Index_BankProductID on bi.ContractCredit(BankProductID);
 		   
 Create table bi.Deposit (DepositID		int primary key
 						,InstrumentID	int not null foreign key references bi.Instrument(InstrumentID)
@@ -172,17 +210,22 @@ Create table bi.Deposit (DepositID		int primary key
 					    ,BankID         int not null foreign key references bi.Client(ClientID)
 						,StateID		int not null foreign key references bi.State(StateID)
 					    ,Currency		int
-						,Number         char(40)
+						,Number         varchar(40)
 					    ,DepAmount		money
 					    ,DepDateFrom	smalldatetime
 					    ,DepDateTo		smalldatetime
 						,Active			int
                         );
+						   
+create index Index_ClientID on bi.Deposit(ClientID);
+create index Index_InstrumentID_StateID on bi.Deposit(InstrumentID, StateID);
 
 Create table bi.InterestType (InterestTypeID	int primary key
-                             ,Brief			char(20)
-				             ,Name			char(160)
+                             ,Brief			varchar(20)
+				             ,Name			varchar(160)
                              );
+						   
+create index Index_Brief on bi.InterestType(Brief);
 
 Create table bi.Interest (InterestID		int primary key
                          ,IstrumentID		int not null foreign key references bi.Instrument(InstrumentID)
@@ -193,4 +236,7 @@ Create table bi.Interest (InterestID		int primary key
 						 ,DateEnd			smalldatetime
 						 ,Active			int
                          );
+						   
+create index Index_ObjectID on bi.Interest(ObjectID);
+create index Index_ObjectID_InterestTypeID on bi.Interest(ObjectID, InterestTypeID);
 
